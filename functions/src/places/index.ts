ee983d83b48df24778733c8efe3aa3eaf4ca337e
Enrico258 <105428037+Enrico258@users.noreply.github.com>
2024-01-17 07:49:11 -0300

    Update index.ts

--- functions/src/places/index.ts
+++ functions/src/places/index.ts
@@ -1,1 +1,34 @@
+import {Request, Response} from "firebase-functions";
+import * as admin from "firebase-admin";
 
+const db = admin.firestore();
+
+interface Session {
+    userId: string;
+    date: string;
+    completed: boolean;
+    choice?: string;
+}
+
+const data = {
+    choice?: string;
+};
+
+const IA = 'https://chat.maritaca.ai/';
+
+const config = {
+  method: 'POST', 
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(data)
+};
+
+fetch(IA, config)
+  .then(resposta => resposta.json())
+  .then(IAanswer => {
+    console.log('Resposta:', IAanswer);
+  })
+  .catch(erro => {
+    console.error('Erro na requisição:', erro);
+  });
